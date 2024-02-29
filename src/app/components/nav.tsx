738fe9db801a6cc2b4cda9f58b38178d0ef8ce53
Vidit Tamrakar <vidit.tamrakar16@gmail.com>
2024-02-29 19:39:43 +0530

    file updated

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -46,7 +46,7 @@ export default function Navbar (){
           <h1 className=" font-sans text-[27px] cursor-pointer sm1:mr-12" onClick={openhome}>Electricart</h1>
           
 
-            <div className=" flex flex-row items-start justify-center cursor-pointer  relative left-5 sm1:left-[-10px]" onClick={opensearch}>
+            <div className=" flex flex-row items-start justify-center cursor-pointer  relative left-5 sm1:left-[-20px]" onClick={opensearch}>
               
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="black"  viewBox="0 0 16 16">
             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
@@ -54,14 +54,14 @@ export default function Navbar (){
             </div>
          <input type="search" placeholder="&nbsp; Search for products brand and more" className=" h-9 w-96 bg-[#CDF5FD] placeholder:text-grey rounded-lg sm1:hidden" onClick={opensearch} />
 
-           <div className=" flex flex-row items-start justify-center cursor-pointer" onClick={opensign}>
+           <div className=" flex flex-row items-start justify-center cursor-pointer sm1:mr-2" onClick={opensign}>
             <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="black" viewBox="0 0 16 16">
             <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
             </svg>
             <h2 className="font-sans ml-3 sm1:hidden">Login</h2>
             </div>
 
-            <div className=" flex flex-row items-start justify-center cursor-pointer " onClick={opencart}>
+            <div className=" flex flex-row items-start justify-center cursor-pointer sm1:mr-2 " onClick={opencart}>
             <div className="flex justify-center items-center h-[15px] w-[15px] rounded-[50%] bg-[red] text-[white]">
                 1
               </div>
@@ -79,8 +79,8 @@ export default function Navbar (){
 
           </div>
 
-            <div onClick={openorder} className={box? "h-[150px] w-[180px] sm1:right-[30px] bg-[white] select-none flex flex-col justify-center items-center fixed right-[500px] top-[60px] shadow-lg rounded-[8px] border" :"hidden"}>
-                <div className="h-[45px] w-[170px] flex justify-start items-center ml-3 cursor-pointer">
+            <div  className={box? "h-[150px] w-[180px] sm1:right-[30px] bg-[white] select-none flex flex-col justify-center items-center fixed right-[500px] top-[60px] shadow-lg rounded-[8px] border" :"hidden"}>
+                <div onClick={openorder} className="h-[45px] w-[170px] flex justify-start items-center ml-3 cursor-pointer">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="gray"  viewBox="0 0 16 16">
                <path fill-rule="evenodd" d="M15.528 2.973a.75.75 0 0 1 .472.696v8.662a.75.75 0 0 1-.472.696l-7.25 2.9a.75.75 0 0 1-.557 0l-7.25-2.9A.75.75 0 0 1 0 12.331V3.669a.75.75 0 0 1 .471-.696L7.443.184l.01-.003.268-.108a.75.75 0 0 1 .558 0l.269.108.01.003zM10.404 2 4.25 4.461 1.846 3.5 1 3.839v.4l6.5 2.6v7.922l.5.2.5-.2V6.84l6.5-2.6v-.4l-.846-.339L8 5.961 5.596 5l6.154-2.461z"/>
                </svg>
