f9c2a2a3e136abba36970b23313c5f0b62591e7f
Vidit Tamrakar <vidit.tamrakar16@gmail.com>
2024-03-13 18:34:24 +0530

    update

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -32,6 +32,11 @@ export default function Navbar (){
     setbox(false)
   }
 
+  const openotify = () => {
+    router.push('/notification')
+    setbox(false)
+  }
+
   const [box ,setbox] = useState<boolean>(false);
 
   const handlebox = () => {
@@ -88,7 +93,7 @@ export default function Navbar (){
                <h2 className=" ml-2">Orders</h2>
                 </div>
 
-                <div className="h-[45px] w-[170px] flex justify-start items-center ml-3  cursor-pointer">
+                <div onClick={openotify} className="h-[45px] w-[170px] flex justify-start items-center ml-3  cursor-pointer">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="gray"  viewBox="0 0 16 16">
                 <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2m.995-14.901a1 1 0 1 0-1.99 0A5 5 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901"/>
                 </svg>
---
+++ src/app/notification/page.tsx
@@ -0,0 +1,7 @@
+export default function Notify (){
+    return (
+        <div className="min-h-[800px] w-[100%] bg-[white]">
+        
+        </div>
+    )
+}
