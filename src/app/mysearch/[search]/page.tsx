49f6f4871769ca62a64456c7a241ae709e25664b
Vidit Tamrakar <vidit.tamrakar16@gmail.com>
2024-03-21 19:46:01 +0530

    update

--- src/app/mysearch/[search]/page.tsx
+++ src/app/mysearch/[search]/page.tsx
@@ -337,7 +337,7 @@ client.query({
     if(!param){
         return(
 
-            <div className=" min-h-[800px] flex flex-col justify-center items-center"> 
+            <div className=" min-h-[800px] flex flex-col justify-center items-center sm1:min-h-[670px]"> 
              <nav className=" w-full h-14 bg-white flex flex-row  top-0 items-center justify-center select-none fixed z-30 border-b sm1:fixed sm1:z-30">
                <h1 className=" font-sans text-[27px] cursor-pointer sm1:hidden relative right-[100px]" onClick={gohome}>Electricart</h1>
             <div className=" flex flex-row items-start justify-center cursor-pointer  relative right-6 sm1:right-[5px]">
--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -46,7 +46,7 @@ export default function Navbar (){
 
     return(
  
-         <nav className=" w-full h-14 bg-white flex flex-row  top-0 items-center justify-center select-none fixed z-20 border-b sm1:fixed sm1:z-20">
+         <nav className=" shadow-lg w-full h-14 bg-white flex flex-row  top-0 items-center justify-center select-none fixed z-20 border-b sm1:fixed sm1:z-20">
           <div className="  w-1/2 h-14 bg-white flex flex-row items-center justify-evenly sm1:w-[300px] ">
           <h1 className=" font-sans text-[27px] cursor-pointer sm1:mr-12" onClick={openhome}>Electricart</h1>
           
@@ -84,7 +84,7 @@ export default function Navbar (){
 
           </div>
 
-            <div  className={box? "h-[150px] w-[180px] sm1:right-[30px] bg-[white] select-none flex flex-col justify-center items-center fixed right-[500px] top-[60px] shadow-lg rounded-[8px] border" :"hidden"}>
+            <div  className={box? "h-[150px] w-[180px] sm1:right-[20px] bg-[white] select-none flex flex-col justify-center items-center fixed right-[500px] top-[60px] shadow-lg rounded-[8px] border" :"hidden"}>
                 <div onClick={openorder} className="h-[45px] w-[170px] flex justify-start items-center ml-3 cursor-pointer">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="gray"  viewBox="0 0 16 16">
                <path fill-rule="evenodd" d="M15.528 2.973a.75.75 0 0 1 .472.696v8.662a.75.75 0 0 1-.472.696l-7.25 2.9a.75.75 0 0 1-.557 0l-7.25-2.9A.75.75 0 0 1 0 12.331V3.669a.75.75 0 0 1 .471-.696L7.443.184l.01-.003.268-.108a.75.75 0 0 1 .558 0l.269.108.01.003zM10.404 2 4.25 4.461 1.846 3.5 1 3.839v.4l6.5 2.6v7.922l.5.2.5-.2V6.84l6.5-2.6v-.4l-.846-.339L8 5.961 5.596 5l6.154-2.461z"/>
